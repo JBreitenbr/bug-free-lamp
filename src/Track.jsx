d2520df712fdf6cddc975bfc69ff4f4547ecda98
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-06-27 15:05:28 +0000

    playing around with tooltip

--- src/Track.jsx
+++ src/Track.jsx
@@ -1,7 +1,8 @@
+import Tooltip from "./Tooltip";
 function Track({ data, kurva }) {
-  const { track, album_pic } = data;
+  const { artist, track, album_pic } = data;
   return (
-    <div className="ml-4 mb-4"><img src={album_pic} className="h-20 w-20 sm:h-32 sm:w-32" style={{border:"1px solid #0f172a"}} /><div className="text-base sm:text-xl md:text-2xl xl:text-3xl mt-2 text-white">{track}</div></div>)
+    <div className="ml-4 mb-4"><Tooltip content={artist} direction="bottom"><img src={album_pic} className="h-20 w-20 sm:h-32 sm:w-32" style={{border:"1px solid #0f172a"}} /></Tooltip><div className="text-base sm:text-xl md:text-2xl xl:text-3xl mt-2 text-white">{track}</div>    </div>)
 }
 
 export default Track;
